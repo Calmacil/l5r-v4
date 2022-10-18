d474999c83f286ab21d5ab8f2da5aa0b129d2459
tleno <thomas.lenoel@ssi-soft.fr>
2022-10-19 01:19:24 +0200

    Ajout des caracs / anneaux

---
+++ src/workers/update_rings.js
@@ -0,0 +1,12 @@
+on('sheet:opened change:constitution change:will change:awareness change:reflex change:force change:perception change:agility change:intelligence', function()
+{
+    getAttrs(['constitution', 'will', 'awareness', 'reflex', 'force', 'perception', 'agility', 'intelligence'], function(v)
+    {
+        setAttrs({
+            earth: Math.min(v['constitution'], v['will']),
+            air: Math.min(v['reflex'], v['awareness']),
+            water: Math.min(v['force'], v['perception']),
+            fire: Math.min(v['agility'], v['intelligence'])
+        })
+    })
+})
